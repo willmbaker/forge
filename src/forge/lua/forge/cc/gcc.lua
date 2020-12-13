0d3af2f3adf9163ade91d4c2bcc4294884e5013a
Will Baker <will.m.baker@gmail.com>
2020-12-13 14:02:40 +1300

    [1] [GCC] I can configure the compilation to halt at the first error.

--- src/forge/lua/forge/cc/gcc.lua
+++ src/forge/lua/forge/cc/gcc.lua
@@ -299,6 +299,10 @@ function gcc.append_compile_flags( toolset, target, flags )
         table.insert( flags, '-Wno-unused-variable' );
         table.insert( flags, '-Wno-unused-but-set-variable' );
     end
+
+    if settings.stop_on_first_error then
+        table.insert( flags, '-Wfatal-errors' );
+    end
 end
 
 function gcc.append_library_directories( toolset, target, library_directories )
